937f303863c3f32c3869879f96407f917c07e16e
Simon Nordlund Magnusson <Simon.Nordlund.Magnusson@gmail.com>
2025-11-14 16:06:09 +0100

    Första commiten på vinterspelet

---
+++ vinterprojekt/Program.cs
@@ -0,0 +1,22 @@
+﻿// Im gonna make a story game about an adventurer who got teleported in to a tower that has different levels and you have to find a way out
+// Firstly i need to make different choices like names and paths to choose.
+// Than i need to make different endings so i have a game that is playable. 
+// I think im going to make about 10 or more endings with different ways to achieve them.
+// but first 3 endings an go to 10 endings.
+// after i have made the endings and story im going to make improvements like maybe some items and damage systems.
+// some improvements on the map maybe a type of map that you can check where you have went or what paths youve gone on.
+
+
+
+Console.WriteLine("You're walking to work on a monday morning when suddenly a wierd circle appears underneath your feet.");
+Console.WriteLine("You're falling through the air at high speeds, the ground a few hundred meters below.");
+Console.WriteLine("Just a few meters abouve the ground now and BOOM!");
+Console.WriteLine("");
+Console.WriteLine("What is your name adventurer?");
+string playername = Console.ReadLine();
+
+
+
+
+
+
